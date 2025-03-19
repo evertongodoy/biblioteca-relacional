8b18fea6937b501707bba77646caf3eba40aff09
Everton Godoy <everton.mgodoy@gmail.com>
2025-03-18 21:25:16 -0300

    Adding initial configurations to the pom.xml file

---
+++ src/main/java/br/senac/sp/biblioteca/BibliotecaRelacionalApplication.java
@@ -0,0 +1,13 @@
+package br.senac.sp.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaRelacionalApplication.class, args);
+	}
+
+}
